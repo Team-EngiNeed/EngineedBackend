7d5363bf3fbadc22ba827b92c240bf41e072596b
Engineed <teamengineed@gmail.com>
2025-02-24 21:18:07 +0800

    Getting ready to push to render

--- EngineedBackend/asgi.py
+++ EngineedBackend/asgi.py
@@ -6,11 +6,12 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
-
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EngineedBackend.settings')
+
+settings_module = 'EngineedBackend.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'EngineedBackend.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_asgi_application()
